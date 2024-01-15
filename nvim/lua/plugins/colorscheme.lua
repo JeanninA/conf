ff233fc637c019e3fbf24d37e117cd7652c45567
Arthur <arthur@Arthurs-MacBook-Pro.local>
2024-01-15 23:38:37 +0100

    Switch to rose-pine-moon

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,8 @@
 return {
-    "rose-pine/neovim",
-    priority = 1000,
-    name = "rose-pine",
+	"rose-pine/neovim",
+	priority = 1000,
+	name = "rose-pine",
+	config = function()
+		vim.cmd.colorscheme("rose-pine-moon")
+	end,
 }
