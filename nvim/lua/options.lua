3c1597f94d047de47a79ade53dfad450a36ca75d
Arthur <ar.jeannin@gmail.com>
2024-02-22 17:03:44 +0100

    Minor change

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,9 +2,10 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 vim.opt.scrolloff = 8
 
