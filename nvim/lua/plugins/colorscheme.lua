99899cd8388dd2131151a861a3230de9b1b48b44
Arthur <ar.jeannin@gmail.com>
2024-03-01 11:21:06 +0100

    Remove rose pine italics

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,10 @@ return {
 	priority = 1000,
 	name = "rose-pine",
 	config = function()
+		require("rose-pine").setup({
+			styles = { italic = false },
+			highlight_groups = { Comment = { italic = true } },
+		})
 		vim.cmd.colorscheme("rose-pine")
 	end,
 }
