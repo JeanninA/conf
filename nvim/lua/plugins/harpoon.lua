288988e3f8eff5ad16312ff77c8120a0ba3c9311
Arthur <ar.jeannin@gmail.com>
2024-03-08 13:08:07 +0100

    Fix error in harpoon keymap

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,21 +1,21 @@
 return {
-    "ThePrimeagen/harpoon",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    config = function()
-        vim.keymap.set("n", "<leader>m", "<cmd>lua require('harpoon.mark').add_file()<cr>")
-        vim.keymap.set("n", "<leader>n", "<cmd>lua require('harpoon.ui').nav_next()<cr>")
-        vim.keymap.set("n", "<leader>p", "<cmd>lua require('harpoon.ui').nav_prev()<cr>")
-        vim.keymap.set("n", "<leader>h", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>")
-        vim.keymap.set("n", "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>")
-        vim.keymap.set("n", "<leader>2", "<cmd>lua require('harpoon.ui').nav_prev(2)<cr>")
-        vim.keymap.set("n", "<leader>3", "<cmd>lua require('harpoon.ui').nav_prev(3)<cr>")
-        vim.keymap.set("n", "<leader>4", "<cmd>lua require('harpoon.ui').nav_prev(4)<cr>")
-        vim.keymap.set("n", "<leader>5", "<cmd>lua require('harpoon.ui').nav_prev(5)<cr>")
-        vim.keymap.set("n", "<leader>6", "<cmd>lua require('harpoon.ui').nav_prev(6)<cr>")
-        vim.keymap.set("n", "<leader>7", "<cmd>lua require('harpoon.ui').nav_prev(7)<cr>")
-        vim.keymap.set("n", "<leader>8", "<cmd>lua require('harpoon.ui').nav_prev(8)<cr>")
-        vim.keymap.set("n", "<leader>9", "<cmd>lua require('harpoon.ui').nav_prev(9)<cr>")
-    end,
+	"ThePrimeagen/harpoon",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>m", "<cmd>lua require('harpoon.mark').add_file()<cr>")
+		vim.keymap.set("n", "<leader>n", "<cmd>lua require('harpoon.ui').nav_next()<cr>")
+		vim.keymap.set("n", "<leader>p", "<cmd>lua require('harpoon.ui').nav_prev()<cr>")
+		vim.keymap.set("n", "<leader>h", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>")
+		vim.keymap.set("n", "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>")
+		vim.keymap.set("n", "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>")
+		vim.keymap.set("n", "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>")
+		vim.keymap.set("n", "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>")
+		vim.keymap.set("n", "<leader>5", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>")
+		vim.keymap.set("n", "<leader>6", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>")
+		vim.keymap.set("n", "<leader>7", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>")
+		vim.keymap.set("n", "<leader>8", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>")
+		vim.keymap.set("n", "<leader>9", "<cmd>lua require('harpoon.ui').nav_file(9)<cr>")
+	end,
 }
