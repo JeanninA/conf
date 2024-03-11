29f13bf2b93f45777a4b8b8643c9393427f0dd6f
Arthur <ar.jeannin@gmail.com>
2024-03-11 14:46:56 +0100

    Point mypy to the proper env

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -11,7 +11,12 @@ return {
 						return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" })
 					end,
 				}),
-				null_ls.builtins.diagnostics.mypy,
+				null_ls.builtins.diagnostics.mypy.with({
+					extra_args = function()
+						local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+						return { "--python-executable", virtual .. "/bin/python3" }
+					end,
+				}),
 				null_ls.builtins.diagnostics.ruff,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
