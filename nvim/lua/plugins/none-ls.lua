c5a2caff43d2a0f2df452b626dfb07208e0035b9
Arthur <ar.jeannin@gmail.com>
2024-04-09 16:53:29 +0200

    Add none ls extra after some deprecations

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,12 +1,15 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d.with({
+				require("none-ls.formatting.eslint_d").with({
 					condition = function(utils)
 						return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" })
 					end,
