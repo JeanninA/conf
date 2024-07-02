4373c49338f66b91085c65cf7874e08858476e0b
Arthur <ar.jeannin@gmail.com>
2024-07-02 15:36:14 +0200

    Add dashboard (startup screen)

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,1 +1,11 @@
-return { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+        exclude = {
+            filetypes = {
+                "dashboard",
+            },
+        },
+    },
+}
