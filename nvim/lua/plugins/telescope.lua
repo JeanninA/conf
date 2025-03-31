11cde96dda95627179839e0930dadd5d8a822276
Arthur Jeannin <ar.jeannin@gmail.com>
2025-03-31 10:39:48 +0200

    Shortcut for telescope buffer list

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -30,6 +30,7 @@ return {
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
             vim.keymap.set("n", "<leader>pf", builtin.git_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
             vim.keymap.set("n", "<leader>pws", function()
                 local word = vim.fn.expand("<cword>")
                 builtin.grep_string({ search = word })
