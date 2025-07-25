9070c4f7d6a863130893722e364a666570a9b2b8
Arthur Jeannin <ar.jeannin@gmail.com>
2025-07-25 10:17:08 +0200

    Show hidden file by default in oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,9 +1,13 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {},
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("oil").setup()
-		vim.keymap.set("n", "-", ":Oil<CR>", { desc = "Open parent directory" })
-	end,
+    "stevearc/oil.nvim",
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("oil").setup({
+            view_options = { show_hidden = true },
+        })
+        vim.keymap.set("n", "-", ":Oil<CR>", { desc = "Open parent directory" })
+        -- to open in a floating window, uncomment the line below
+        -- vim.keymap.set("n", "-", ":Oil --float<CR>", { desc = "Open parent directory" })
+    end,
 }
