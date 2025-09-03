d874626e1f8d16287acb4719bda64aa102cc3519
Arthur Jeannin <ar.jeannin@gmail.com>
2025-09-03 15:12:08 +0200

    Remove noice, replace notify by fidget

--- nvim/lua/plugins/notify.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "rcarriga/nvim-notify",
-    config = function()
-        local config = require("notify")
-        config.setup({
-            background_colour = "#000000", -- for 100% transparency
-        })
-    end,
-}
