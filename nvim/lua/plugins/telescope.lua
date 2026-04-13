ed869ade4a2c79a541601aac32815d4a5ee6ee34
Arthur Jeannin <ar.jeannin@gmail.com>
2026-04-13 10:32:24 +0200

    Fix telescope and treesitter

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,8 +4,8 @@ return {
     },
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        branch = "0.1.x",
+        -- tag = "0.1.5",
+        -- branch = "0.1.x",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local builtin = require("telescope.builtin")
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,5 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            local config = require("nvim-treesitter.configs")
-            config.setup({
-                auto_install = true,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end,
-    },
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
 }
